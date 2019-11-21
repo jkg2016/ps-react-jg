842548eb336b54edc55bf7da1a8415369b5d6090
SharePoint Development <spdevelopment@contoso.com>
2019-11-21 13:17:06 -0500

    Setting up documentation

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function HelloWord({message}) {
     return <div>Hello {message}</div>
 }
 
+HelloWord.protoTypes = {
+    message: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+    message:'World'
+}
 export default HelloWord;
