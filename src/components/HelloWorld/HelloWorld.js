8985b076e2637bec139eac55d852bd1696265ac3
SharePoint Development <spdevelopment@contoso.com>
2019-11-21 13:33:47 -0500

    Working code

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function HelloWord({message}) {
+function HelloWorld({message}) {
     return <div>Hello {message}</div>
 }
 
-HelloWord.protoTypes = {
+HelloWorld.protoTypes = {
     message: PropTypes.string
 };
 
 HelloWorld.defaultProps = {
     message:'World'
 }
-export default HelloWord;
+export default HelloWorld;
